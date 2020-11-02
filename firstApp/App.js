36355c8e5a2556cb906cdc07e06508670184e343
Cynicos <9davita9@gmail.com>
2020-11-02 16:37:09 +0400

    init 2

--- firstApp/App.js
+++ firstApp/App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState("შაქრო");
+
+  const handleClick= () => {
+    setName("ზაქრო")
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.body}><Text style={styles.textStyle}>{name}</Text></View>
+      
+      <TouchableHighlight onPress={handleClick} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>დამაკლიკე</Text>
+          </View>
+        </TouchableHighlight>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +32,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  body: {
+    backgroundColor: '#808080',
+    padding: 30,
+    margin: 20,
+    width: '100%'
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 20,
+    color: 'white'
+  },
+  header: {
+    backgroundColor: '#808080',
+    padding: 30,
+  },
+  textStyle: {
+    fontSize: 30,
+    color: 'white',
+  }
 });
